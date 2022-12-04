3ef004c31bbae308babe5133661c159471983c94
Benny <64857370+Bwah-creator@users.noreply.github.com>
2022-12-04 18:17:19 +0100

    Update and rename client.java to Client.java

---
+++ Client.java
@@ -0,0 +1,52 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public abstract class Client {
+	
+	public static void main (String[] args) {
+		Socket client = null;
+		
+		
+		try{
+			client = new Socket("localhost", 4999);
+			DataOutputStream raus = new DataOutputStream(client.getOutputStream());
+			raus.writeUTF(args[0]);
+			BufferedReader leser = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String leserS = leser.readLine();
+		
+			System.out.println(leserS);
+			System.out.println("Bitte Groesse und Farbe getrennt mit : eingeben. Beispiel eingabe = TSHIRT:Groesse:Farbe");
+			Scanner eingabeGF = new Scanner(System.in);
+			String gF = eingabeGF.nextLine();
+			
+			
+			
+		
+			raus.writeUTF( gF);
+			
+			BufferedReader leser2 = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String leserS2 = leser2.readLine();
+			
+			System.out.println(leserS2);
+			
+			
+			Scanner b = new Scanner(System.in);
+			String be = b.nextLine();
+			raus.writeUTF(be);
+			
+			BufferedReader leser3 = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String leserS3 = leser3.readLine();
+			System.out.println(leserS3);
+			}
+			catch(Exception e){
+				System.out.println(e);
+				}
+		
+	}
+	
+	
+}
+
+
+
